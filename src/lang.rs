4cfea9d3476b3f335f6ddefbc9e899d422635e7e
Aaron Sikes <aaron@sikes.io>
2020-01-04 17:54:27 -0500

    temp

--- src/lang.rs
+++ src/lang.rs
@@ -62,16 +62,40 @@ fn parse_name(input: &str) -> IResult<&str, TypeSpec, VerboseError<&str>> {
     map(take_while(|c: char| c.is_alphanumeric() || c == '-' || c == '_' || c == '?' || c == '!' || c == '/'), |n| TypeSpec::Name(n))(input)
 }
 
+// fn parse_hash(input: &str) -> IResult<&str, TypeSpec, VerboseError<&str>> {
+//     preceded(separated_pair(
+//     // at least 1 byte group (2 hex chars)
+//     map(multi1(
+// }
+
+use nom::character::complete::hex_digit1;
+use nom::bytes::complete::take;
+
+fn hex_byte(input: &str) -> IResult<&str, u8, VerboseError<&str>> {
+    let (rest, digits) = take(2u8)(input)?;
+    let (_, digits) = hex_digit1(digits)?;
+    Ok((rest, u8::from_str_radix(digits, 16).unwrap()))
+}
+
+// fn decimal_integer(kk
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn test_hex_bytes() {
+        assert_eq!(hex_byte("12ff"), Ok(("ff", 18)));
+    }
+
+    #[test]
     fn simple_type_parsing() {
         let nil = "Nil";
         let short = "#facebabe_00552288:12";
-        let long = "#cafebabe_12341234_cafebabe_12341234_cafebabe_12341234_cafebabe_12341234:3";
+        let long = "#cafebabe12341234cafebabe12341234cafebabe12341234cafebabe12341234:3";
+        // hyphens and underscores
+        let long_seperated = "#cafebabe-12341234_cafebabe_12341234-cafebabe_12341234_cafebabe_12341234:3";
 
         let prefix = hex!("facebabe 00552288");
         let hash = hex!("cafebabe 12341234 cafebabe 12341234 cafebabe 12341234 cafebabe 12341234");
@@ -81,8 +105,9 @@ mod tests {
         let sum = "(yes | no {reason: Text} | other Value)";
 
         assert_eq!(parse_type(nil).assert(nil), TypeSpec::Name("Nil"));
-        // assert_eq!(parse_type(short).assert(short), TypeSpec::ShortHash(prefix.to_vec(), 12));
-        // assert_eq!(parse_type(long).assert(long), TypeSpec::Hash(Hash(hash), 3));
+        assert_eq!(parse_type(short).assert(short), TypeSpec::ShortHash(prefix.to_vec(), 12));
+        assert_eq!(parse_type(long).assert(long), TypeSpec::Hash(Hash(hash), 3));
+        assert_eq!(parse_type(long_seperated).assert(long_seperated), TypeSpec::Hash(Hash(hash), 3));
         // assert_eq!(parse_type("{}").assert("{}"), TypeSpec::Product(Vec::new()));
         // assert_eq!(parse_type(single_product).assert(single_product), TypeSpec::Product(vec![("field", TypeSpec::Name("Value"))]));
         // assert_eq!(parse_type(multiple_product).assert(multiple_product), TypeSpec::Product(vec![("key1", TypeSpec::Name("Value")), ("key2", TypeSpec::Name("Value"))]));
